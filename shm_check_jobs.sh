a4c61b4d9438ff7ae65e01b673fe4b0cb0beb63a
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2024-09-20 20:50:05 +0800

    Update shm_check_jobs.sh
    Fix

--- shm_check_jobs.sh
+++ shm_check_jobs.sh
@@ -12,7 +12,7 @@ cd ${DOCKER_COMPOSE_PATH}
 echo "SELECT event, status FROM spool WHERE event LIKE '%Jobs%'" | docker-compose exec -T mysql /bin/bash -c 'MYSQL_PWD=${MYSQL_ROOT_PASSWORD} mysql -u root shm' > ${DOCKER_COMPOSE_PATH}/spool.job
 MESSAGE=$(grep '^{"kind": "Jobs"' ${DOCKER_COMPOSE_PATH}/spool.job | sed 's/"kind": "Jobs", "title": //g;s/"//g;s/ method://g;s/|//g;s/ *//g;s///g;s/SUCCESS/\n<b>SUCCESS<\/b> \n/g')
 sleep 2
-curl -s -X  POST -d chat_id=$CHAT_ID -d parse_mode=HTML -d text="$MESSAGE" https://api.telegram.org/$BOT_TOKEN/sendMessage
+curl -s -X  POST -d chat_id=$CHAT_ID -d parse_mode=HTML -d text="$MESSAGE" https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
 
 #пример cron для выполнения каждые 24 часа:
 #  3 */24 * * * sudo sh /opt/shm/check_spool_new.sh
