940e385ce1d88d12249cb17d8de9e505c8131628
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2025-03-30 23:03:28 +0800

    Create download_template_from_shm.sh

---
+++ download_template_from_shm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Загрузка шаблона из SHM в текущую директорию
+
+# Название шаблона
+SHM_TEMPLATE="my_template"
+# Учетные данные для авторизации - логин:пароль
+CREDENTAILS="admin:xxxxxxx"
+# Адрес
+HOST="https://admin.xxxxxx.ru/"
+
+# Загрузка
+curl -s -u "$CREDENTAILS" \
+     -X "GET" \
+     -H "Content-Type: text/html; charset=utf-8" \
+     $HOST/shm/v1/admin/template/$SHM_TEMPLATE \
+     -o "$SHM_TEMPLATE.tt"
